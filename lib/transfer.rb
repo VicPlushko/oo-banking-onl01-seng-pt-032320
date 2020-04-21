ba34f0b94d1d12f3e6072872efbd8d1d1f065242
Victor Plushko <vicplushko33@gmail.com>
2020-04-21 07:44:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,8 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-    
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 end
