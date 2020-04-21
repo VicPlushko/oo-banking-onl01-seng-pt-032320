c72ad2f6b07efac5fe2190a7d8f83b91e26322c3
Victor Plushko <vicplushko33@gmail.com>
2020-04-21 07:43:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,10 +13,11 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
     
     end
   end
