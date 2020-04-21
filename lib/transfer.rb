cee436503eacce6ff8406d92a3b937aa0ee819c0
Victor Plushko <vicplushko33@gmail.com>
2020-04-21 06:43:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,9 @@
 class Transfer
   attr_accessor :transfer, :sender, :receiver
   
-  def initialize(transfer)
+  def initialize(transfer, sender, receiver)
     @transfer = transfer
+    @sender = sender
+    @receiver = receiver
   end
 end
