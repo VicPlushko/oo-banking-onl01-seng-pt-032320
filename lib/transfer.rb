2d295828a4c7d03adb3cf14d93f15066d68c29c6
Victor Plushko <vicplushko33@gmail.com>
2020-04-21 07:28:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,14 +13,11 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance < @amount
-       @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+    if 
     else
-      @sender.balance > @amount && @status == "pending"
-       @sender.balance -= @amount
-       @receiver.balance += @amount
-       @status = "complete"
+      @sender.balance < @amount
+       @status = "rejected"
+       "Transaction rejected. Please check your account balance."
     end
   end
 end
