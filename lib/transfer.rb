46130014c67f5c3e2e454c5213e6ef52d607249e
Victor Plushko <vicplushko33@gmail.com>
2020-04-21 07:55:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,12 +13,5 @@ class Transfer
   end
   
   def execute_transaction
-   if valid? && sender.balance > amount && self.status == "pending"
-      sender.withdrawal(self.amount)
-      receiver.deposit(self.amount)
-      self.status = "complete"
-    else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
+   if 
 end
